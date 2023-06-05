fba9dd474232a7a38c209e98f26db4a9be61bb9b
sbeuran <samuel.beuran98@gmail.com>
2023-06-05 22:44:37 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SearchResultsPage from './components/SearchResultsPage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-      <SearchResultsPage />
-    </div>
+    <Router>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search-results" element={<SearchResultsPage />} />
+    </Router>
   );
 }
 
