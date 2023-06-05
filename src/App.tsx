9f56538d4aa01d9f9cb46c99e5ca1066d825238a
sbeuran <samuel.beuran98@gmail.com>
2023-06-05 22:32:12 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SearchResultsPage from './components/SearchResultsPage';
 
 function App() {
   return (
-    <Router>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/search-results" element={<SearchResultsPage />} />
-    </Router>
+    <div>
+      <HomePage />
+      <SearchResultsPage />
+    </div>
   );
 }
 
